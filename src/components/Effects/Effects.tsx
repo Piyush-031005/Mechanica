50d2882d065eccdadd53aeed67112c0be52875f5
Piyush-031005 <piyushpunera107@gmail.com>
2026-08-30 20:20:12 +0530

    style: apply DepthOfField and Vignette to emulate cinematic lens properties

--- src/components/Effects/Effects.tsx
+++ src/components/Effects/Effects.tsx
@@ -1,19 +1,27 @@
 "use client";
 
-import { EffectComposer, Noise, Bloom, ChromaticAberration, Scanline } from "@react-three/postprocessing";
+import { EffectComposer, Noise, Bloom, ChromaticAberration, Scanline, DepthOfField, Vignette } from "@react-three/postprocessing";
 import { BlendFunction } from "postprocessing";
 import * as THREE from "three";
 
 export function Effects() {
   return (
     <EffectComposer multisampling={8}>
-      {/* Clean Blueprint Grain */}
-      <Noise blendFunction={BlendFunction.OVERLAY} opacity={0.03} />
+      {/* Cinematic Lens Depth */}
+      <DepthOfField 
+        focusDistance={0.02} 
+        focalLength={0.05} 
+        bokehScale={2} 
+        height={480} 
+      />
+
+      {/* Cinematic Film Grain */}
+      <Noise blendFunction={BlendFunction.OVERLAY} opacity={0.02} />
 
       {/* Holographic Scanline for that tech feel */}
       <Scanline blendFunction={BlendFunction.OVERLAY} density={1.5} opacity={0.05} />
 
-      {/* Very subtle bloom, NO blowouts */}
+      {/* Subtle bloom, only on extremely bright (emissive) surfaces */}
       <Bloom 
         luminanceThreshold={0.5} 
         luminanceSmoothing={0.9} 
@@ -21,12 +29,15 @@ export function Effects() {
         mipmapBlur 
       />
 
-      {/* Premium lens effect, no screen breaking */}
+      {/* Premium digital lens aberration */}
       <ChromaticAberration 
         offset={new THREE.Vector2(0.0005, 0.0005)} 
         radialModulation={false}
         modulationOffset={0}
       />
+
+      {/* Deep Space Vignette to focus the eye */}
+      <Vignette eskil={false} offset={0.1} darkness={1.1} blendFunction={BlendFunction.NORMAL} />
     </EffectComposer>
   );
 }
